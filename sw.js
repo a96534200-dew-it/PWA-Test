58a72c8ed891b37b531fc10e2354bd11ddf9ae89
a96534200-dew-it <a96534200@gmail.com>
2025-11-25 14:09:11 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', event => {
         caches.open('v1').then(cache => {
             return cache.addAll([
                 '/',
-                '/index.html' ,
-                '/styles.css' ,
-                '/main.js' ,
-                '/manifest.json' ,
+                '/index.html',
+                '/styles.css',
+                '/main.js',
+                '/manifest.json',
                 '/icon.png'
             ]);
         })
